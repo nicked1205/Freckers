f2a9b15b632807a9bbdcd3ff2127d02f3401ff03
ADUDESLAPTOP\ncta2 <nick.nguyen2150@gmail.com>
2025-03-27 20:45:33 +1100

    finalize heuristics part 2

--- search/heuristic.py
+++ search/heuristic.py
@@ -7,55 +7,62 @@ def calculate_heuristics(node: TreeNode, visited: list[TreeNode]):
         child: TreeNode
         for child in node.child_dict.values():
             if child:
+                print(child)
                 distances = []
                 child_dict = child.child_dict
                 if child_dict[Direction.Left]:
                     distance = 0
-                    distances.append(test_jump(child_dict[Direction.Left], distance))
+                    distances.append(test_jump(child_dict[Direction.Left], distance, []))
                 if child_dict[Direction.Right]:
                     distance = 0
-                    distances.append(test_jump(child_dict[Direction.Right], distance))
+                    distances.append(test_jump(child_dict[Direction.Right], distance, []))
                 if child_dict[Direction.DownLeft]:
                     distance = 1
                     if child_dict[Direction.DownLeft].jumping:
                         distance = 2
-                    distances.append(test_jump(child_dict[Direction.DownLeft], distance))
+                    distances.append(test_jump(child_dict[Direction.DownLeft], distance, []))
                 if child_dict[Direction.Down]:
                     distance = 1
                     if child_dict[Direction.Down].jumping:
                         distance = 2
-                    distances.append(test_jump(child_dict[Direction.Down], distance))
+                    distances.append(test_jump(child_dict[Direction.Down], distance, []))
                 if child_dict[Direction.DownRight]:
                     distance = 1
                     if child_dict[Direction.DownRight].jumping:
                         distance = 2
-                    distances.append(test_jump(child_dict[Direction.DownRight], distance))
-                print(child)
-                print(max(distances, default=0))
+                    distances.append(test_jump(child_dict[Direction.DownRight], distance, []))
+                print('Conclusion:')
+                print(distances)
+                print('\n')
                 child.set_heuristic(max(distances, default=0))
                 calculate_heuristics(child, visited)
 
-def test_jump(node: TreeNode, distance: int):
+def test_jump(node: TreeNode, distance: int, visited):
     distances = [distance]
-    if node.jumping:
-        child_dict = node.child_dict
-        if child_dict[Direction.Left]:
-            distances.append(test_jump(child_dict[Direction.Left], distance))
-        if child_dict[Direction.Right]:
-            distances.append(test_jump(child_dict[Direction.Right], distance))
-        if child_dict[Direction.DownLeft]:
-            if child_dict[Direction.DownLeft].jumping:
-                distances.append(test_jump(child_dict[Direction.DownLeft], distance + 2))
-            else:
-                distances.append(test_jump(child_dict[Direction.DownLeft], distance + 1))
-        if child_dict[Direction.Down]:
-            if child_dict[Direction.Down].jumping:
-                distances.append(test_jump(child_dict[Direction.Down], distance + 2))
-            else:
-                distances.append(test_jump(child_dict[Direction.Down], distance + 1))
-        if child_dict[Direction.DownRight]:
-            if child_dict[Direction.DownRight].jumping:
-                distances.append(test_jump(child_dict[Direction.DownRight], distance + 2))
-            else:
-                distances.append(test_jump(child_dict[Direction.DownRight], distance + 1))
+    if node.coord not in visited:
+        visited.append(node.coord)
+        if node.jumping:
+            child_dict = node.child_dict
+            if child_dict[Direction.Left]:
+                distances.append(test_jump(child_dict[Direction.Left], distance, visited))
+            if child_dict[Direction.Right]:
+                distances.append(test_jump(child_dict[Direction.Right], distance, visited))
+            if child_dict[Direction.DownLeft]:
+                if child_dict[Direction.DownLeft].jumping:
+                    distances.append(test_jump(child_dict[Direction.DownLeft], distance + 2, visited))
+                else:
+                    distances.append(test_jump(child_dict[Direction.DownLeft], distance + 1, visited))
+            if child_dict[Direction.Down]:
+                if child_dict[Direction.Down].jumping:
+                    distances.append(test_jump(child_dict[Direction.Down], distance + 2, visited))
+                else:
+                    distances.append(test_jump(child_dict[Direction.Down], distance + 1, visited))
+            if child_dict[Direction.DownRight]:
+                if child_dict[Direction.DownRight].jumping:
+                    distances.append(test_jump(child_dict[Direction.DownRight], distance + 2, visited))
+                else:
+                    distances.append(test_jump(child_dict[Direction.DownRight], distance + 1, visited))
+        print('----------------')
+        print(node)
+        print(distances)
     return max(distances, default=0)
--- search/tree.py
+++ search/tree.py
@@ -22,8 +22,16 @@ class TreeNode():
     def set_heuristic(self, heuristic):
         self.heuristic = heuristic
 
+    def coord_search(self, coord):
+        if self.coord == coord:
+            return True
+        return False
+    
+    def __str__(self):
+        return f"Coord: {self.coord} Jumping: {self.jumping}"
+
 def expand_tree(board: dict[Coord, CellState], visited: list[TreeNode], coord: Coord, root: TreeNode):
-    visited.append(coord)
+    visited.append(root)
     for dir in [Direction.Left, Direction.DownLeft, Direction.Down, Direction.DownRight, Direction.Right]:
         try:
             new_coord = Coord(coord.r + dir.r, coord.c + dir.c)
@@ -39,16 +47,25 @@ def expand_tree(board: dict[Coord, CellState], visited: list[TreeNode], coord: C
             cell_state_jump = board.get(new_jump_coord)
             
             if cell_state_jump == CellState.LILY_PAD:
-                new_node = TreeNode(cell_state, new_jump_coord, True)
-                root.add_child(dir, new_node)
-                if new_jump_coord not in visited:                    
+                if new_jump_coord not in [node.coord for node in visited]:   
+                    new_node = TreeNode(cell_state, new_jump_coord, True)
+                    root.add_child(dir, new_node)                 
                     expand_tree(board, visited, new_jump_coord, new_node)
+                else:
+                    for node in visited:
+                        if node.coord_search(new_jump_coord):
+                            root.add_child(dir, node)
+                    
 
         elif cell_state == CellState.LILY_PAD:
-            new_node = TreeNode(cell_state, new_coord, False)
-            root.add_child(dir, new_node)
-            if new_coord not in visited:                    
+            if new_coord not in [node.coord for node in visited]:   
+                new_node = TreeNode(cell_state, new_coord, False)
+                root.add_child(dir, new_node)                 
                 expand_tree(board, visited, new_coord, new_node)
+            else:
+                for node in visited:
+                    if node.coord_search(new_coord):
+                        root.add_child(dir, node)
 
 # def generate_tree(board: dict[Coord, CellState], visited: list[TreeNode], coord: Coord, root: TreeNode):
 #     visited.append(coord)
