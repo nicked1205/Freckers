a4be66fe3323e7b6ec9746091588148d0ad9fc37
ADUDESLAPTOP\ncta2 <nick.nguyen2150@gmail.com>
2025-03-27 17:01:29 +1100

    tree.py modification

--- search/tree.py
+++ search/tree.py
@@ -8,73 +8,63 @@ class TreeNode():
         self.state = state
         self.coord = coord
         self.child_dict = {
-                        "left": None,
-                        "right": None,
-                        "down": None,
-                        "downLeft": None,
-                        "downRight": None,
+                        Direction.Left: None,
+                        Direction.DownLeft: None,
+                        Direction.Down: None,
+                        Direction.DownRight: None,
+                        Direction.Right: None,
                         }
 
     def add_child(self, dir_vector, node):
-        match dir_vector:
-            case (0, -1):
-                self.child_dict["left"] = node
-            case (0, 1):
-                self.child_dict["right"]= node
-            case (1, 0):
-                self.child_dict["down"] = node
-            case (1, -1):
-                self.child_dict["downLeft"] = node
-            case (1, 1):
-                self.child_dict["downRight"] = node
+        self.child_dict[dir_vector] = node
 
 def expand_tree(board: dict[Coord, CellState], visited, coord: Coord, root: TreeNode):
     visited.append(coord)
-    for m, n in [(1, 0), (0, 1), (1, -1), (1, 1), (0, -1)]:
+    for dir in [Direction.Left, Direction.DownLeft, Direction.Down, Direction.DownRight, Direction.Right]:
         try:
-            new_coord = Coord(coord.r + m, coord.c + n)
+            new_coord = Coord(coord.r + dir.r, coord.c + dir.c)
         except:
             continue
         cell_state = board.get(new_coord)
 
         if cell_state == CellState.BLUE:
             try:
-                new_jump_coord = Coord(coord.r + 2*m, coord.c + 2*n)
+                new_jump_coord = Coord(coord.r + 2*dir.r, coord.c + 2*dir.c)
             except:
                 continue
             cell_state_jump = board.get(new_jump_coord)
             
             if new_jump_coord not in visited and cell_state_jump == CellState.LILY_PAD:
                 new_node = TreeNode(0, cell_state, new_jump_coord)
-                root.add_child((m, n), new_node)
+                root.add_child(dir, new_node)
                 expand_tree(board, visited, new_jump_coord, new_node)
 
         elif new_coord not in visited and cell_state == CellState.LILY_PAD:
             new_node = TreeNode(0, cell_state, new_coord)
-            root.add_child((m, n), new_node)
+            root.add_child(dir, new_node)
             expand_tree(board, visited, new_coord, new_node)
 
 def generate_tree(board: dict[Coord, CellState], visited, coord: Coord, root: TreeNode):
     visited.append(coord)
-    for m, n in [(1, 0), (0, 1), (1, -1), (1, 1), (0, -1)]:
+    for dir in [Direction.Left, Direction.DownLeft, Direction.Down, Direction.DownRight, Direction.Right]:
         try:
-            new_coord = Coord(coord.r + m, coord.c + n)
+            new_coord = Coord(coord.r + dir.r, coord.c + dir.c)
         except:
             continue
         cell_state = board.get(new_coord)
 
         if cell_state == CellState.BLUE:
             try:
-                new_jump_coord = Coord(coord.r + 2*m, coord.c + 2*n)
+                new_jump_coord = Coord(coord.r + 2*dir.r, coord.c + 2*dir.c)
             except:
                 continue
             cell_state_jump = board.get(new_jump_coord)
             
             if new_jump_coord not in visited and cell_state_jump == CellState.LILY_PAD:
                 new_node = TreeNode(0, cell_state, new_jump_coord)
-                root.add_child((m, n), new_node)
+                root.add_child(dir, new_node)
 
         elif new_coord not in visited and cell_state == CellState.LILY_PAD:
             new_node = TreeNode(0, cell_state, new_coord)
-            root.add_child((m, n), new_node)
+            root.add_child(dir, new_node)
     return visited          
